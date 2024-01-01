2a6305a9012eb3aa2bd2fef18d755bf1bb18c5e9
kphed <kpcrypto69@gmail.com>
2024-01-01 14:01:40 -0500

    Filter out accounts with zero balances, refine logic

--- src/index.js
+++ src/index.js
@@ -14,6 +14,28 @@ const {
 const erc20Abi = require("./abi/erc20.json");
 const excludedAddresses = require("./excludedAddresses");
 
+const computeAccountBalances = (logs) => {
+    return logs.reduce((acc, { args: { from, to, amount } }) => {
+        // Exclude the zero address, and project and advisor contract addresses.
+        if (from !== ZERO_ADDRESS && !excludedAddresses[from]) {
+            // `from` should always have a balance since we're accruing from the deployment block.
+            acc[from] -= amount;
+
+            // If the token sender no longer has a balance, remove them.
+            if (acc[from] === BigInt(0)) delete acc[from];
+        }
+
+        if (to !== ZERO_ADDRESS && !excludedAddresses[to]) {
+            // Assign the account balance to zero if it doesn't exist.
+            if (acc[to] === undefined) acc[to] = BigInt(0);
+
+            acc[to] += amount;
+        }
+
+        return acc;
+    }, {});
+};
+
 const getTransferLogs = async () => {
     try {
         const brr = await client.getContractEvents({
@@ -49,24 +71,4 @@ const getTransferLogs = async () => {
     } catch (err) {
         console.error(err);
     }
-};
-
-const computeAccountBalances = (logs) => {
-    return logs.reduce((acc, { args: { from, to, amount } }) => {
-        // Exclude the zero address, and project and advisor contract addresses.
-        if (from !== ZERO_ADDRESS && !excludedAddresses[from]) {
-            // Assign the account balance to zero if it doesn't exist.
-            if (acc[from] === undefined) acc[from] = BigInt(0);
-
-            acc[from] -= amount;
-        }
-
-        if (to !== ZERO_ADDRESS && !excludedAddresses[to]) {
-            if (acc[to] === undefined) acc[to] = BigInt(0);
-
-            acc[to] += amount;
-        }
-
-        return acc;
-    }, {});
 };
