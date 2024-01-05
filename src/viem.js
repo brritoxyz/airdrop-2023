81346f4833268bcc36f80cbbc2114bc9331776b6
kphed <kpcrypto69@gmail.com>
2024-01-05 08:35:07 -0500

    Configure and export wallet client if pk present, update logs (new viem2 format)

--- src/viem.js
+++ src/viem.js
@@ -1,8 +1,14 @@
-const { createPublicClient, http } = require("viem");
+const { createPublicClient, createWalletClient, http } = require("viem");
+const { privateKeyToAccount } = require("viem/accounts");
 const { base } = require("viem/chains");
 
-module.exports = createPublicClient({
-    chain: base,
-    transport: http(process.env.RPC_URL ?? ""),
-});
-
+module.exports = process.env.TOKEN_DISTRIBUTOR_PK
+    ? createWalletClient({
+          account: privateKeyToAccount(process.env.TOKEN_DISTRIBUTOR_PK),
+          chain: base,
+          transport: http(process.env.RPC_URL ?? ""),
+      })
+    : createPublicClient({
+          chain: base,
+          transport: http(process.env.RPC_URL ?? ""),
+      });
